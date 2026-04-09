6b3b768630cb1abdd7e44f35a8d1aa1d727c7f21
marquach <c.marquardt@me.com>
2026-04-09 12:10:44 +0200

    Refactor understand_data_process.py to streamline imports, enhance data handling, and improve dataset processing for rainfall visualization

--- pilot/understand_data_process.py
+++ pilot/understand_data_process.py
@@ -1,52 +1,49 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-import requests
-import itertools
-import bisect
-import os
+from torchvision import transforms
 
-
-from pilot.helper import parse_gregorian_day, parse_utc_time, download_mch_hdf5, is_timepoint
 
 from pysteps import rcparams, io
 import datetime as dt
 from pysteps.utils import conversion, dimension, transformation
-from pysteps.visualization import plot_precip_field, animations
+import pysteps.visualization as pyvis
+from pilot.helper import rain_precipitation_mch, download_mch_hdf5
 
-date = dt.datetime.strptime("202604051955", "%Y%m%d%H%M")
+
+
+date = dt.datetime.strptime("20260402", "%Y%m%d")
 BASEURL = "https://data.geo.admin.ch/ch.meteoschweiz.ogd-radar-precip"
+dates = [date + dt.timedelta(days=x) for x in range(4)]
+dates
 window = 36 # 7*5mim=35 min windows
 # otherwise pull whole day
 
-download_mch_hdf5('rzc', BASEURL, date, window= window)
+#[download_mch_hdf5('rzc', BASEURL, date, window= 0) for date in dates]
 
-data_source = rcparams.data_sources["mch"]
+#download_mch_hdf5('rzc', BASEURL, date, window= window)
+
 root_path = 'pilot/data/'
-path_fmt = data_source['path_fmt']
-fn_pattern = 'rzc%y%j%H%Mvl.001'
-fn_ext = 'h5'
-importer_name = 'mch_hdf5'
-timestep = data_source["timestep"]
-
-
 date_search = dt.datetime.strptime("202604052015", "%Y%m%d%H%M")
-# Find the frame in the archive for the specified date
-fns = io.find_by_date(
-    date_search, root_path, path_fmt, fn_pattern, fn_ext, timestep, num_prev_files=1, num_next_files=20)
-
-importer_kwargs = rcparams.data_sources['mch']["importer_kwargs"]
-importer_kwargs.update({'product': 'rzc'})
-# Read the data from the archive
-importer = io.get_method(importer_name, "importer")
-R, _, metadata = io.read_timeseries(fns, importer, **importer_kwargs)
 
 # data validation
-temp = np.lib.stride_tricks.sliding_window_view(metadata['timestamps'], 2)
-np.all(np.diff(temp) == dt.timedelta(seconds = 300))
+#temp = np.lib.stride_tricks.sliding_window_view(metadata['timestamps'], 2)
+#np.all(np.diff(temp) == dt.timedelta(seconds = 300))
 
-R.shape
-R[-1,:,:]
-plot_precip_field(R[-1,:,:], geodata=metadata)
 
-animations.animate(R)
+
+ds = rain_precipitation_mch(root_path, date = date_search, window_size=4, target_size=1)
+
+print(f"Dataset len: {len(ds)}")
+x, y = ds[0]
+print(f"x.shape: {x.shape}, y.shape: {y.shape}")  # Erwartet: (4,H,W), (1,H,W)
+print(f"Metadata keys: {list(ds.metadata.keys())}")
+
+pyvis.plot_precip_field(x[-1])
+plt.show()
+pyvis.animations.animate(x)
+
+
+from pysteps.nowcasts import steps
+steps.forecast
+nowcast = steps.forecast(ds.data[:8],) 
